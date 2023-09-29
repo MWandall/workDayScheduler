0608b1858728203172e007d134a2573d73b80437
MWandall <144394104+MWandall@users.noreply.github.com>
2023-09-28 19:01:16 -0500

    added JS for color change function

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,39 @@
-var timeDisplayEl = $('#clock')
-var currentDayEl = $('#currentDay')
+
 var saveButton = $('.saveBtn')
-var hourNine = $('hour-9')
-var hourTen = $('hour-10')
-var hourEleven = $('hour-11')
-var hourTwelve = $('hour-12')
-var hourOne = $('hour-1')
-var hourTwo = $('hour-2')
-var hourThree = $('hour-3')
-var hourFour = $('hour-4')
-var hourFive = $('hour-5')
+
+var hourNine = $('#hour-9')
+var hourTen = $('#hour-10')
+var hourEleven = $('#hour-11')
+var hourTwelve = $('#hour-12')
+var hourOne = $('#hour-13')
+var hourTwo = $('#hour-14')
+var hourThree = $('#hour-15')
+var hourFour = $('#hour-16')
+var hourFive = $('#hour-17')
+
 var textInput = $('.description')
+
+
+var currentTimeHours = dayjs().hour();
+
+
+function timeColors() {
+    textInput.each(function () {
+        var timeBlock = $(this).attr("id");
+   
+    if (currentTimeHours > timeBlock) {
+        $(this).addClass("past");
+    } else if (currentTimeHours === timeBlock) {
+        $(this).addClass("present");
+        $(this).remove("past");
+    } else {
+        $(this).addClass("future");
+        $(this).remove("present");
+        $(this).remove("past");
+    }
+    
+})};
+
 
 
 
@@ -29,17 +52,17 @@ $(function () {
     // *var saveButton = $('.saveBtn') written at top, just a reminder of names
     // *var hourNine = $('hour-9')
 
-    saveButton.addEventListener('click', function() {
-        var text = hourNine.value;
-        localStorage.setItem('userTextInput', JSON.stringify(text))
+    // saveButton.on('click', function() {
+    //     var text = hourNine.value;
+    //     localStorage.setItem('userTextInput', JSON.stringify(text))
 
-    });
+    // });
 
-    var storedText = localStorage.getItem(text);
-    if (storedText) {
-        var pulledText = JSON.parse(savedText);
-        hourNine.value = pulledText;
-    }
+    // var storedText = localStorage.getItem(text);
+    // if (storedText) {
+    //     var pulledText = JSON.parse(savedText);
+    //     hourNine.value = pulledText;
+    // }
 
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
@@ -47,12 +70,27 @@ $(function () {
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
 
+    function timeColors() {
+        textInput.each(function () {
+            var timeBlock = $(this).attr("id");
+       
 
-    // if dayjs() === hourNine
-    //                 hourTen etc
+        if (currentTimeHours === timeBlock) {
+                $(this).addClass("present");
+        
+        } else if (currentTimeHours < timeBlock) {
+            $(this).addClass("future");
+            $(this).remove("present");
+        } else {
+            $(this).addClass("past");
+            $(this).remove("present");
+            $(this).remove("future");
+        }
+        
+    })};
+    
 
-
-    //*maybe .diff? even and odd hour numbers trigger color changes?
+   
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
@@ -62,43 +100,43 @@ $(function () {
 
 
     // TODO: Add code to display the current date in the header of the page.
+    var timeDisplayEl = $('#clock')
+    var currentDayEl = $('#currentDay')
+  
+    function displayTime() {
+  
+    var currentTime = dayjs().format('MMM DD [-] hh:mm:ss a');
+    timeDisplayEl.text(currentTime);
+    dayOfWeek = dayjs().format('dddd')
+    currentDayEl.text(dayOfWeek)
+    }
+
+    
+  displayTime();
+  setInterval(displayTime, 1000);
+
+  timeColors()
+
+
   });
 
 
 
-  function displayTime() {
-
-  var currentTime = dayjs().format('MMM DD [-] hh:mm:ss a');
-  timeDisplayEl.text(currentTime);
-  dayOfWeek = dayjs().format('dddd')
-  currentDayEl.text(dayOfWeek)
-  }
-
-  displayTime();
-setInterval(displayTime, 1000);
 
 
 
+//   var timeDisplayEl = $('#clock')
+//   var currentDayEl = $('#currentDay')
 
+//   function displayTime() {
 
-// var today = dayjs();
-// var hourNum = today.diff(beginningOfYear, 'hour');
-// var evenOddHour = hourNum % 2 === 1;    
+//   var currentTime = dayjs().format('MMM DD [-] hh:mm:ss a');
+//   timeDisplayEl.text(currentTime);
+//   dayOfWeek = dayjs().format('dddd')
+//   currentDayEl.text(dayOfWeek)
+//   }
 
+//   displayTime();
+// setInterval(displayTime, 1000);
 
-// var hourCard [
-//     "$(".hour-9")"
-
-
-// ]
-
-
-// **example from 26 storage todos 
-// var todos = [];
-//   // Get stored todos from localStorage
-//   var storedTodos = JSON.parse(localStorage.getItem("todos"));
-//     // If todos were retrieved from localStorage, update the todos array to it
-//     if (storedTodos !== null) {
-//         todos = storedTodos;
-//       }
-
+// timeColors()
